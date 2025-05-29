3886e1ad69f7880de95c9c8b160efd340a6ec6bc
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-29 13:13:31 +0900

    fix memos view.py

--- contact/views.py
+++ contact/views.py
@@ -7,11 +7,16 @@ from contact.serializers import ContactSerializer
 class ContactCreateView(generics.ListCreateAPIView):
       serializer_class = ContactSerializer
       permission_classes = [permissions.IsAuthenticated]
-      
+
+      permission_classes = [permissions.AllowAny]
+
       def get_queryset(self):
         # 自分のメモだけ取得
         return ContactCreateView.objects.filter(user=self.request.user)
-      
+
       def perform_create(self, serializer):
         # 作成時に自動でログインユーザーを紐付け
         serializer.save(user=self.request.user)
+
+def get_queryset(self):
+   return Concat.objects.filter(user = self.request.user)
