8f7977209ca0979911b3e532c08b657bf03f8cca
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-29 16:38:01 +0900

    fix to the url.py for config setting

--- notices/urls.py
+++ notices/urls.py
@@ -3,5 +3,5 @@ from notices.views import NoticesCreateView, NoticesRetrieveUpdateDestroyView
 
 urlpatterns = [
   path('notices/',NoticesCreateView.as_view(), name='notices-list-create'),
-  path('notices/<int:pk>/', NoticesRetrieveUpdateDestroyView.as_view(), name='notices-detail'),  # GET, PUT, DELETE by id
+  path('notices/<int:pk>/', NoticesRetrieveUpdateDestroyView.as_view(), name='notices-detail'),
 ]
