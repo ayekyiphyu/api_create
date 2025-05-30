2c68f8b65bd9759cf21da53f8b4b187a8bc816b0
AYEKYIPHYU <ayekyiphyu@yuime.co.jp>
2025-05-30 13:41:03 +0900

    add to the book calendar api

---
+++ book_calendar/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics, permissions
+from .models import Booking
+from .serializers import BookingSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class BookingListCreateView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class BookingDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
