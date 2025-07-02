d855c413176eea60d94679d7c2667aba52b4a7b8
ayekyiphyu <ayekyiphyu@yuime.co.jp>
2025-07-02 16:20:16 +0900

    fix:weather models add

--- weather/serializers.py
+++ weather/serializers.py
@@ -4,5 +4,13 @@ from weather.models import Weather
 class WeatherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Weather
-        fields = ['id', 'name', 'email', 'message', 'created_at', 'updated_at']
+        fields = [
+            'id',
+            'name',
+            'lat',
+            'lng',
+            'country',
+            'created_at',
+            'updated_at',
+        ]
         read_only_fields = ['id', 'created_at', 'updated_at']
